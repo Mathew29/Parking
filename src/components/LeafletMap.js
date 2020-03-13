b0cc6ff8dcd9657a43dae7b4b4c7c59cffc4cb23
Mathew Akre <mwakre29@gmail.com>
2020-03-13 12:57:37 -0700

    Create LeafletMap component

---
+++ src/components/LeafletMap.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import L from 'leaflet';
+
+export default class LeafletMap extends Component {
+
+}
